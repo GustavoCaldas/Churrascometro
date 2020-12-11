0ee9b083d2e164e1ecb1ec16d55ec433b9116a01
Gustavo Caldas <gustavocaldes@gmail.com>
2020-12-11 14:02:41 -0300

    update script

--- scripts/script.js
+++ scripts/script.js
@@ -15,39 +15,43 @@ function churrascometro(){
     let cerveja = null;
     let refrigerante_agua = null;
 
-    if(duracao <= 6 && duracao > 0){
-        /*
-        *  400 g carne por pessoa
-        *  1200ml de cerveja por adulto
-        *  1000ml por pessoa
-        *  criancas contam como 0,5
-        */
-        carne = 400 * pessoas;
-        cerveja = 1200 * adultos;
-        refrigerante_agua = 1000 * pessoas;
-        outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
-        outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
-        outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
-    }else if(duracao > 6){
-        /* 
-        *  650 g carne por pessoa
-        *  2000ml de cerveja por adulto
-        *  1500ml por pessoa
-        *  criancas contam como 0,5 
-        */
-        carne = 650 * pessoas;
-        cerveja = 2000 * adultos;
-        refrigerante_agua = 1500 * pessoas;
-        outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
-        outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
-        outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
+    if(adultos >= 0 && criancas >=0){
+        if(duracao <= 6 && duracao > 0){
+            /*
+            *  400 g carne por pessoa
+            *  1200ml de cerveja por adulto
+            *  1000ml por pessoa
+            *  criancas contam como 0,5
+            */
+            carne = 400 * pessoas;
+            cerveja = 1200 * adultos;
+            refrigerante_agua = 1000 * pessoas;
+            outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
+            outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
+            outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
+        }else if(duracao > 6){
+            /* 
+            *  650 g carne por pessoa
+            *  2000ml de cerveja por adulto
+            *  1500ml por pessoa
+            *  criancas contam como 0,5 
+            */
+            carne = 650 * pessoas;
+            cerveja = 2000 * adultos;
+            refrigerante_agua = 1500 * pessoas;
+            outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
+            outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
+            outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
+        }
+    }else{
+        window.alert("Dado inválido!")
     }
 }
 
 function toKg(e1){
-    return e1 / 1000;
+    return Math.ceil(e1 / 1000);
 }
 
 function toL(e1){
-    return e1 / 1000;
+    return Math.ceil(e1 / 1000);
 }
