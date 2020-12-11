002b7301f93f2f0db0fbf59f796ade130889a737
Gustavo Caldas de Souza <gustavocaldes@gmail.com>
2020-12-11 10:43:33 -0300

    update scriot

--- scripts/script.js
+++ scripts/script.js
@@ -25,9 +25,9 @@ function churrascometro(){
         carne = 400 * pessoas;
         cerveja = 1200 * adultos;
         refrigerante_agua = 1000 * pessoas;
-        outputs[0].innerHTML = "Carne: " + toKG(carne);
-        outputs[1].innerHTML = "Cerveja: " + cerveja;
-        outputs[2].innerHTML = "Refrigerante/agua: " + refrigerante_agua;
+        outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
+        outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
+        outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
     }else if(duracao > 6){
         /* 
         *  650 g carne por pessoa
@@ -38,12 +38,16 @@ function churrascometro(){
         carne = 650 * pessoas;
         cerveja = 2000 * adultos;
         refrigerante_agua = 1500 * pessoas;
-        outputs[0].innerHTML = "Carne: " + carne;
-        outputs[1].innerHTML = "Cerveja: " + cerveja;
-        outputs[2].innerHTML = "Refrigerante/agua: " + refrigerante_agua;
+        outputs[0].innerHTML = "Carne: " + toKg(carne) + " Kilos";
+        outputs[1].innerHTML = "Cerveja: " + toL(cerveja) + " Litros";
+        outputs[2].innerHTML = "Refrigerante/agua: " + toL(refrigerante_agua) + " Litros";
     }
 }
 
-function toKG(e1){
+function toKg(e1){
+    return e1 / 1000;
+}
+
+function toL(e1){
     return e1 / 1000;
 }
